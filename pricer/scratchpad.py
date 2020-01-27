dad5bef5556e44a77f5bb9fac40ecf3989ca057b
HalnyGit <haliniakm@gmail.com>
2020-01-27 02:49:59 +0100

    hc

--- pricer/scratchpad.py
+++ pricer/scratchpad.py
@@ -1,96 +1,37 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Nov 13 21:01:50 2019
-
-@author: Komp
-"""
-
-import pandas as pd
 import numpy as np
-import datetime
+import scipy as sp
+import matplotlib.pyplot as plt
+import scipy.interpolate
 
-def is_leap(d):
-    '''
-    d: date
-    return: True if year of the date 'd' is leap, False otherwise
-    '''
-    try:
-        datetime.date(d.year, 2, 29)
-    except:
-        return False
-    return True
+t=np.array([0, 10, 15, 20, 22.5, 30])
+v=np.array([0, 227.04, 362.78, 517.35, 602.97, 901.67,])
 
-def days_between(d1, d2):
-    '''
-    d1: date
-    d2: date
-    return: integer, number of days between dates d1 and d2
-    '''
-    return (d2 - d1) / datetime.timedelta(days=1)
+plt.close('all')
+plt.figure(1)
+plt.grid(axis='both', which='major', color=[166/255,166/255, 166/255],
+         linestyle='-', linewidth=2)
+plt.grid(axis='both', which='minor', color=[166/255, 166/255, 166/255],
+         linestyle=':', linewidth=2)
+plt.minorticks_on()
+plt.xlabel('time (s)')
+plt.ylabel('volocity (m/s)')
+plt.title('Projectile of a rocket')
+plt.scatter(t, v, 50, [255/255, 0/255, 0/255], label='orignal data')
+plt.legend(loc='upper left')
+plt.plot([16,16], [0, np.max(v)], color=[0/255, 176/255, 80/255],
+         linestyle=':', linewidth=2)
+t16=16
+v16_nearest=v[2]
+plt.scatter(t16, v16_nearest, 50, color=[0/255, 112/255, 192/255],
+            label='nearest point interpolation')
 
-
-def dcf_actact(d1, d2):
-    '''
-    d1: date, d2: date
-    return: day count fraction in ACT/ACT ISDA convention
-    '''
-    base1=366 if is_leap(d1) else 365
-    base2=366 if is_leap(d2) else 365
-    eoy = datetime.date(d1.year, 12, 31)
-    return (((days_between(d1, eoy) + 1) / base1) + ((days_between(eoy, d2) - 1) / base2))
-
+fvcubic=sp.interpolate.interp1d(t, v, kind='cubic')
+tnew=np.arange(start=0, stop=30, step=1)
+vcubic=fvcubic(tnew)
+plt.plot(tnew, vcubic, color=[0/255, 112/255,192/255])
 
 
 
-dates = {'A':[datetime.date(2019, 11, 15), datetime.date(2019, 12, 15)],
-         'B':[datetime.date(2020, 11, 15), datetime.date(2020, 12, 15)]     
-        }
-
-df = pd.DataFrame(dates)
-
-df['C'] = df['A'].apply(lambda x: x - datetime.timedelta(days=1))
-df['D'] = (df['B'] - df['A']) / datetime.timedelta(days=1)
-
-t=[]
-for i in range(len(df['A'])):
-    n = dcf_actact(df['A'][i], df['B'][i])
-    t.append(n)
 
 
 
-df['E'] = df.apply(lambda x: dcf_actact(x['A'], x['B']), axis=1)
-
-
-df['F'] = dcf_actact(df['A'], df['B'])
-    
-
-# =============================================================================
-# 
-# 
-# 
-# weekmask_pd = 'Mon Tue Wed Thu Fri'
-# holidays_pd = ['2019-11-15', '2019-12-09', '2019-12-13', '2020-02-01']
-# bday_pd = pd.offsets.CustomBusinessDay(holidays=holidays_pd, weekmask=weekmask_pd, offset=datetime.timedelta(weeks=1))
-# start_date = pd.Timestamp('2019-11-13')
-# end_date = pd.Timestamp('2020-11-13')
-# 
-# #dts = pd.bdate_range(start_date, end_date, freq=(bday_pd))
-# dts = pd.bdate_range(start_date, end_date, freq=bday_pd)
-# dts
-# 
-# dts2 = pd.bdate_range(start_date, end_date, freq='CBMS', weekmask=weekmask_pd, holidays=holidays_pd) + pd.offsets.Day(start_date.day-1)
-# dts2
-# 
-# dts3 = pd.bdate_range(start_date, end_date, freq='CBMS', weekmask=weekmask_pd, holidays=holidays_pd)+ datetime.timedelta(days=start_date.day-1)
-# dts3
-# 
-# dts3 = pd.bdate_range(start_date, end_date, freq='CBMS+{}'.format(start_date.day), weekmask=weekmask_pd, holidays=holidays_pd)
-# dts3
-# 
-# dts4 = pd.bdate_range(start_date, end_date, freq='11SMS', weekmask=weekmask_pd, holidays=holidays_pd)
-# dts4
-# 
-# dts5 = pd.date_range(start_date, end_date,freq='1SMS+13')
-# dts5
-# 
-# =============================================================================
